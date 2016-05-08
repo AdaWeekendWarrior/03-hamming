94586d078e5c5ad2aa6cd33226f58b7f30e0c7aa
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-07 19:28:33 -0700

    now with answer

--- hamming.rb
+++ hamming.rb
@@ -5,18 +5,32 @@ class Hamming
       raise ArgumentError.new("The lengths of the strands must be equal.")
     end
 
-    array1 = seq1.split(//)
-    array2 = seq2.split(//)
-    count = 0
-
-    array1.each_with_index do |item,index|
-      if item == array2[index]
-        count = count + 0
-      else
-        count = count + 1
-      end
-    end
-    return count
+    @array1 = seq1.split(//)
+    @array2 = seq2.split(//)
+    @count = 0
+    next_elements
+    count_differences(@element1,@element2)
   end
-  
+
+  def self.count_differences(element1,element2)
+    if @element1 == nil
+      return @count
+    end
+
+    if element1 != element2
+      @count += 1
+      next_elements
+      count_differences(@element1,@element2)
+    else
+      @count += 0
+      next_elements
+      count_differences(@element1,@element2)
+    end
+  end
+
+  def self.next_elements
+    @element1 = @array1.shift
+    @element2 = @array2.shift
+  end
+
 end
