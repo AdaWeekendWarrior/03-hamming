2ef3fc27a6dad041b3b7189c6ebfe3457526a219
Nadine Curry <nlcurry@gmail.com>
2016-05-10 10:29:13 -0700

    Random

--- hamming.rb
+++ hamming.rb
@@ -4,11 +4,11 @@ class Hamming
     self.compute_helper(var1, var2)
   end
 
-   def self.compute_helper(var1,var2)
-     return 0 if var1 == nil
-     var1[0] == var2[0] ? value = 0 : value = 1
-     value + self.compute_helper(var1[1..-1], var2[1..-1])
-   end
+ def self.compute_helper(var1,var2)
+    return 0 if var1 == nil
+    var1[0] == var2[0] ? value = 0 : value = 1
+    value += self.compute_helper(var1[1..-1], var2[1..-1])
+ end
 end
 
   # def self.compute (var1, var2)
